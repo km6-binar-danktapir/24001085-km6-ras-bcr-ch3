ec45ff52dd35fc138632d6d33b6be63d3355a595
Rasyad Jabbar Poncosewoyo <rasyadponcosewoyo@gmail.com>
2024-03-19 18:13:31 +0700

    tried to implement logic for fetching cars data from backend (unfinished)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,38 @@
-import App from "./App.js";
+import { getTimeStamp } from "../../utils/cars-utils.js";
 
 const cariMobilBtn = document.getElementById("cari-mobil-btn");
 
-cariMobilBtn.addEventListener("click", (event) => {
+async function getCars() {
+    const response = await fetch("/cars");
+    return response.json();
+}
+
+async function getFilteredCars() {
+    /**
+     * filter berdasarkan input fields
+     */
+    const driverType = document.getElementById("pilih-driver").value;
+    const selectedDate = document.getElementById("date-picker").value;
+    const selectedPickUpTime = document.getElementById("waktu-jemput").value;
+    const passengersCapacity =
+        document.getElementById("jumlah-penumpang").value;
+    const pickUpTimeStamp = getTimeStamp(selectedDate, selectedPickUpTime);
+
+    const response = await fetch(
+        `/cars?driverType=${driverType}&pickUpTimestamp=${pickUpTimeStamp}&passengerCapacity=${passengersCapacity}`
+    );
+    return response.json();
+}
+
+cariMobilBtn.addEventListener("click", async (event) => {
     const displayedCarsSection = document.getElementById(
         "displayed-cars-section"
     );
-    const filteredCars = App.filter();
+    const filteredCars = await getFilteredCars();
 
     event.preventDefault();
 
-    if (filteredCars.length != 0) {
+    if (filteredCars.length !== 0) {
         // if there are cars with corresponding filters, then do:
         displayedCarsSection.innerHTML = `<div class="row g-4" id="displayed-cars"></div>`;
 
@@ -23,3 +45,5 @@ cariMobilBtn.addEventListener("click", (event) => {
         displayedCarsSection.innerHTML = "";
     }
 });
+
+await getCars();
---
+++ utils/cars-utils.js
@@ -0,0 +1,16 @@
+function getTimeStamp(date, time) {
+    const dateParts = date.split("-");
+    const timeParts = time.split(":");
+    const timestamp = new Date(
+        parseInt(dateParts[0]),
+        parseInt(dateParts[1] - 1),
+        parseInt(dateParts[2]),
+        parseInt(timeParts[0]),
+        parseInt(timeParts[1])
+    );
+    console.log(timestamp);
+
+    return timestamp;
+}
+
+module.exports = { getTimeStamp };
